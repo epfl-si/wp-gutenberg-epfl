57ec540b906d302559d4ade2d34dfd5138f2d326
Julien Delasoie <julien.delasoie@epfl.ch>
2019-08-09 18:24:08 +0200

    Set all text fields into the editor

--- src/epfl-contact/index.js
+++ src/epfl-contact/index.js
@@ -1,9 +1,10 @@
+
 import contactIcon from './contact-icon'
 
 const { __ } = wp.i18n;
 
 const {
-	registerBlockType,
+    registerBlockType,
 } = wp.blocks;
 
 const {
@@ -21,11 +22,11 @@ const {
 const { Fragment } = wp.element;
 
 registerBlockType( 'epfl/contact', {
-	title: __( 'EPFL Contact', 'wp-gutenberg-epfl'),
-	description: 'v1.0.0',
-	icon: contactIcon,
-	category: 'common',
-	attributes: {
+    title: __( 'EPFL Contact', 'wp-gutenberg-epfl'),
+    description: 'v1.0.0',
+    icon: contactIcon,
+    category: 'common',
+    attributes: {
         introduction: {
             type: 'text',
         },
@@ -34,16 +35,16 @@ registerBlockType( 'epfl/contact', {
             default: false,
         },
         timetable1: {
-			type: 'text',
+            type: 'text',
         },
         timetable2: {
-			type: 'text',
+            type: 'text',
         },
         timetable3: {
-			type: 'text',
+            type: 'text',
         },
         timetable4: {
-			type: 'text',
+            type: 'text',
         },
         information1: {
             type: 'text',
@@ -60,11 +61,11 @@ registerBlockType( 'epfl/contact', {
         map_query: {
             type: 'text',
         }
-	},
-	supports : {
-		customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
-	},
-	edit: ( props ) => {
+    },
+    supports : {
+        customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
+    },
+    edit: ( props ) => {
         const { attributes, className, setAttributes } = props
 
         return (
@@ -77,93 +78,102 @@ registerBlockType( 'epfl/contact', {
                             onChange={ gray_wrapper => setAttributes( { gray_wrapper } ) }
                         />
                     </PanelBody>
-                    <PanelBody title='Introduction'>
-                        <TextareaControl
+                </InspectorControls>
+                <div className={ className }>
+                    <h3>Contact</h3>
+                    <label><strong>Introduction</strong></label>
+                    <hr />
+                    <RichText
                             label={ __('Text introducing the contact informations', 'wp-gutenberg-epfl') }
                             value={ attributes.introduction }
                             onChange={ introduction => setAttributes( { introduction } ) }
-                            help={ __('(optional)', 'wp-gutenberg-epfl') }
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __( 'Timetable', 'wp-gutenberg-epfl' ) }>
-                        <TextControl
-                            value={ attributes.timetable1 }
-                            onChange={ timetable1 => setAttributes( { timetable1 } ) }
-                            help={ __('Ex. : Monday to thursday <b>09:00 am > 18:00 pm</b>', 'wp-gutenberg-epfl') }
-                        />
-                        <TextControl
-                            value={ attributes.timetable2 }
-                            onChange={ timetable2 => setAttributes( { timetable2 } ) }
-                        />
-                        <TextControl
-                            value={ attributes.timetable3 }
-                            onChange={ timetable3 => setAttributes( { timetable3 } ) }
-                        />
-                        <TextControl
-                            value={ attributes.timetable4 }
-                            onChange={ timetable4 => setAttributes( { timetable4 } ) }
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __( 'Information', 'wp-gutenberg-epfl' ) }>
-                        <TextareaControl
-                            value={ attributes.information1 }
-                            onChange={ information1 => setAttributes( { information1 } ) }
-                        />
-                        <TextareaControl
-                            value={ attributes.information2 }
-                            onChange={ information2 => setAttributes( { information2 } ) }
-                        />
-                        <TextareaControl
-                            value={ attributes.information3 }
-                            onChange={ information3 => setAttributes( { information3 } ) }
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __( 'Map', 'wp-gutenberg-epfl' ) }>
-                        <TextControl
-                            label={ __('A room/place to get more information', 'wp-gutenberg-epfl') }
-                            value={ attributes.map_query }
-                            onChange={ map_query => setAttributes( { map_query } ) }
-                            help={ __('A room for example. See plan.epfl.ch for the right text of your room.', 'wp-gutenberg-epfl') }
-                        />
-                    </PanelBody>
-                </InspectorControls>
-                <div className={ className }>
-                    <label><strong>Information1</strong></label>
+                            tagName="div"
+                            multiline="p"
+                            placeholder={ __('Our information desk is the central point of contact for any information.', 'wp-gutenberg-epfl') }
+                            keepPlaceholderOnFocus = { true }
+                            allowedFormats={[]}
+                    />
+                    <label><strong>{ __( 'Timetable', 'wp-gutenberg-epfl' ) }</strong></label>
+                    <hr />
+                    <RichText
+                        tagName="div"
+                        multiline={ false }
+                        value={ attributes.timetable1 }
+                        onChange={ timetable1 => setAttributes( { timetable1 } ) }
+                        placeholder={ __('Monday to thursday 09:00 am > 18:00 pm', 'wp-gutenberg-epfl') }
+                        keepPlaceholderOnFocus = { true }
+                        className="timetable1"
+                    />
+                    <hr />
+                    <RichText
+                        tagName="div"
+                        multiline={ false }
+                        value={ attributes.timetable2 }
+                        onChange={ timetable2 => setAttributes( { timetable2 } ) }
+                        className="timetable2"
+                    />
+                    <hr />
+                    <RichText
+                        tagName="div"
+                        multiline={ false }
+                        value={ attributes.timetable3 }
+                        onChange={ timetable3 => setAttributes( { timetable3 } ) }
+                        className="timetable3"
+                    />
+                    <hr />
+                    <RichText
+                        tagName="div"
+                        multiline={ false }
+                        value={ attributes.timetable4 }
+                        onChange={ timetable4 => setAttributes( { timetable4 } ) }
+                        className="timetable4"
+                    />
+                    <label><strong>Information</strong></label>
                     <hr />
                     <RichText
                         tagName="div"
                         multiline="p"
-                        placeholder={ __( 'Write your content here', 'wp-gutenberg-epfl' ) }
+                        placeholder={ __( 'An email ?', 'wp-gutenberg-epfl' ) }
+                        keepPlaceholderOnFocus = { true }
                         value={ attributes.information1 }
                         className="information1"
                         onChange={ information1 => setAttributes( { information1 } ) }
                     />
-                    
-                    <label><strong>Information2</strong></label>
                     <hr />
                     <RichText
                         tagName="div"
                         multiline="p"
-                        placeholder={ __( 'Write your content here', 'wp-gutenberg-epfl' ) }
+                        placeholder={ __( 'A phone number ?', 'wp-gutenberg-epfl' ) }
+                        keepPlaceholderOnFocus = { true }
                         value={ attributes.information2 }
                         className="information2"
                         onChange={ information2 => setAttributes( { information2 } ) }
                     />
-                    <label><strong>Information3</strong></label>
                     <hr />
                     <RichText
                         tagName="div"
                         multiline="p"
-                        placeholder={ __( 'Write your content here', 'wp-gutenberg-epfl' ) }
+                        placeholder={ __( 'An address ?', 'wp-gutenberg-epfl' ) }
+                        keepPlaceholderOnFocus = { true }
                         value={ attributes.information3 }
                         className="information3"
                         onChange={ information3 => setAttributes( { information3 } ) }
                     />
+                    <hr />
+                    <label><strong>{ __( 'Map', 'wp-gutenberg-epfl' ) }</strong></label>
+                    <TextControl
+                        label={ __('A room/place shown in the interactive map:', 'wp-gutenberg-epfl') }
+                        value={ attributes.map_query }
+                        onChange={ map_query => setAttributes( { map_query } ) }
+                        placeholder={ 'CM 0 361.2' }
+                        help={ <a target="_blank" href="plan.epfl.ch">{ __('See plan.epfl.ch for the right name of your room.', 'wp-gutenberg-epfl') }</a> }
+                    />
                 </div>
             </Fragment>
-		)
-	},
-	save: ( props ) => {
-		return null;
-	},
+        )
+    },
+    save: ( props ) => {
+        return null;
+    },
 } );
+
