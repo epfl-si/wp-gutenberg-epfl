6cd11fca9155ac193f705f5e3ca19c2507a680c0
Julien Delasoie <julien.delasoie@epfl.ch>
2019-08-14 16:34:03 +0200

    Fix conflict

--- frontend/init.php
+++ frontend/init.php
@@ -31,11 +31,8 @@ require_once 'epfl-post-highlight/controller.php';
 require_once 'epfl-infoscience-search/controller.php';
 require_once 'epfl-social-feed/controller.php';
 require_once 'epfl-contact/controller.php';
-<<<<<<< HEAD
 require_once 'epfl-schools/controller.php';
-=======
 require_once 'epfl-card/controller.php';
->>>>>>> master
 
 // Exit if accessed directly.
 if ( ! defined( 'ABSPATH' ) ) {
