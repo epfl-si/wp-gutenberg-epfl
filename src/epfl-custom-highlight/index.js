5dd2a5131fd0e9294920940fe22797e3f4783704
Samuel <samuel.dubuis@netplus.ch>
2019-08-23 14:56:23 +0200

    ergonomisation look de Custom Highlight

--- src/epfl-custom-highlight/index.js
+++ src/epfl-custom-highlight/index.js
@@ -7,13 +7,13 @@ const {
 } = wp.blocks;
 
 const {
-    MediaUpload,
+	MediaUpload,
 	InspectorControls,
 } = wp.editor;
 
 const {
-    PanelBody,
-    TextControl,
+	PanelBody,
+	TextControl,
 	TextareaControl,
 	RadioControl,
 } = wp.components;
@@ -27,9 +27,9 @@ registerBlockType( 'epfl/custom-highlight', {
 	category: 'common',
 	attributes: {
 		title: {
-            type: 'string',
-        },
-        description: {
+			type: 'string',
+		},
+		description: {
 			type: 'string',
 		},
 		link: {
@@ -37,98 +37,89 @@ registerBlockType( 'epfl/custom-highlight', {
 		},
 		buttonLabel: {
 			type: 'string',
-        },
-        imageId: {
-            type: 'number',
-        },
-        imageUrl: {
-            type: 'string',
-        },
-		layout: {
-            type: 'string',
-            default: 'right',
 		},
-		
+		imageId: {
+			type: 'number',
+		},
+		imageUrl: {
+			type: 'string',
+		},
+		layout: {
+			type: 'string',
+			default: 'right',
+		},
+
 	},
 	supports : {
 		customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
 	},
 	edit: ( props ) => {
-        const { attributes, className, setAttributes } = props
-        
-        function onImageSelect(imageObject) {
+		const { attributes, className, setAttributes } = props
+
+		function onImageSelect(imageObject) {
 			setAttributes({
-                imageId: imageObject.id,
-                imageUrl: imageObject.url
+				imageId: imageObject.id,
+				imageUrl: imageObject.url
 			})
 		}
 
 		let optionsLayoutList = [
-            { value: 'right', label: __('Right', 'wp-gutenberg-epfl')},
-            { value: 'bottom', label: __('Bottom', 'wp-gutenberg-epfl')},
-            { value: 'left', label: __('Left', 'wp-gutenberg-epfl')},
-        ];
-        
-        return (
-            <Fragment>
-                <InspectorControls>
-                    <PanelBody title={ __('Title', 'wp-gutenberg-epfl') }>
-                        <TextControl
-                            value={ attributes.title }
-                            onChange={ title => setAttributes( { title } ) }
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __('Description', 'wp-gutenberg-epfl') }>
-                        <TextareaControl
-                            value={ attributes.description }
-                            onChange={ description => setAttributes( { description } ) }
-                        />
-                    </PanelBody>
-					<PanelBody title={ __('Link', 'wp-gutenberg-epfl') }>
-                        <TextControl
-                            value={ attributes.link }
-                            onChange={ link => setAttributes( { link } ) }
-                        />
-                    </PanelBody>
-					<PanelBody title={ __('Button label', 'wp-gutenberg-epfl') }>
-                        <TextControl
-                            value={ attributes.buttonLabel }
-                            onChange={ buttonLabel => setAttributes( { buttonLabel } ) }
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __('Select Image', 'wp-gutenberg-epfl') }>
-                        <MediaUpload
-                            onSelect={onImageSelect}
-                            type="image"
-                            value={attributes.imageId}
-                            render={({ open }) => (
-                                <div>
-                                    <img style={ {maxHeight: '200px'} } src={ attributes.imageUrl } />
-                                    <button onClick={open}>
-                                    { __('Upload Image!', 'wp-gutenberg-epfl') }
-                                    </button>
-                                    <div style={ {marginTop: '5px'} }>{ __('Please, select a image. Recommended image size: 1920x1080', 'wp-gutenberg-epfl') }</div>
-                                </div>
-                            )}
-                        />
-				    </PanelBody>
-					<PanelBody title={ __( 'Layout', 'wp-gutenberg-epfl' ) }>
-                    <RadioControl
-                        label={ __("Select a layout", 'wp-gutenberg-epfl') }
-                        selected={ attributes.layout }
-                        options={ optionsLayoutList }
-                        onChange={ layout => setAttributes( { layout } ) }
-                    />
-                </PanelBody>
-                </InspectorControls>
-                <div className={ className }>
-                    <div id="preview-box">
-                        <h2>EPFL Custom Highlight</h2>
-                        <div className="helper">{ __('Please fill the fields in the right-hand column', 'wp-gutenberg-epfl') }</div>
-                    </div>
-                </div>
-            </Fragment>
-		)		
+		{ value: 'right', label: __('Right', 'wp-gutenberg-epfl')},
+		{ value: 'bottom', label: __('Bottom', 'wp-gutenberg-epfl')},
+		{ value: 'left', label: __('Left', 'wp-gutenberg-epfl')},
+		];
+
+		return (
+		<Fragment>
+			<InspectorControls>
+				<hr/>
+				<RadioControl
+					label={ __("Select a layout", 'wp-gutenberg-epfl') }
+					selected={ attributes.layout }
+					options={ optionsLayoutList }
+					onChange={ layout => setAttributes( { layout } ) }
+				/>
+			</InspectorControls>
+			<div className={ className }>
+				<h2>EPFL Custom Highlight</h2>
+				<TextControl
+					label={ __('Title', 'wp-gutenberg-epfl')}
+					value={ attributes.title }
+					onChange={ title => setAttributes( { title } ) }
+				/>
+				<TextareaControl
+					label={ __('Description', 'wp-gutenberg-epfl')}
+					value={ attributes.description }
+					onChange={ description => setAttributes( { description } ) }
+				/>
+				<TextControl
+					label={ __('Link', 'wp-gutenberg-epfl') }
+					value={ attributes.link }
+					onChange={ link => setAttributes( { link } ) }
+				/>
+				<TextControl
+					label={ __('Button label', 'wp-gutenberg-epfl') }
+					value={ attributes.buttonLabel }
+					onChange={ buttonLabel => setAttributes( { buttonLabel } ) }
+				/>
+				<MediaUpload
+					label={ __('Select Image', 'wp-gutenberg-epfl') }
+					onSelect={onImageSelect}
+					type="image"
+					value={attributes.imageId}
+					render={({ open }) => (
+				<div>
+					<img style={ {maxHeight: '200px'} } src={ attributes.imageUrl } />
+					<button onClick={open}>
+						{ __('Upload Image!', 'wp-gutenberg-epfl') }
+					</button>
+					<div style={ {marginTop: '5px'} }>{ __('Please, select a image. Recommended image size: 1920x1080', 'wp-gutenberg-epfl') }</div>
+				</div>
+				)}
+				/>
+			</div>
+		</Fragment>
+		)
 	},
 	save: ( props ) => {
 		return null;
