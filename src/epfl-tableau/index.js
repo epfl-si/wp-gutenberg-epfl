d867ecdd0be0155bc18787c1c4b4c1a55f99bfd7
Samuel <samuel.dubuis@netplus.ch>
2019-08-30 10:14:35 +0200

    ergonomisation et relooking epfl-tableau

--- src/epfl-tableau/index.js
+++ src/epfl-tableau/index.js
@@ -43,45 +43,41 @@ registerBlockType( 'epfl/tableau', {
 	},
 	edit: ( props ) => {
         const { attributes, className, setAttributes } = props
-        
+
         return (
             <Fragment>
                 <InspectorControls>
-                    <PanelBody title={ __('EPFL Tableau content', 'wp-gutenberg-epfl') }>
+					<hr/>
+						<h4>For an URL</h4>
+                        <TextControl
+							label={ __('Width', 'wp-gutenberg-epfl') }
+                            value={ attributes.width }
+                            onChange={ width => setAttributes( { width } ) }
+                        />
+                        <TextControl
+							label={ __('Height', 'wp-gutenberg-epfl') }
+                            value={ attributes.height }
+                            onChange={ height => setAttributes( { height } ) }
+                        />
+                </InspectorControls>
+                <div className={ className }>
+                    <h2>EPFL TABLEAU</h2>
                         <TextareaControl
+							label={ __('EPFL Tableau content', 'wp-gutenberg-epfl') }
                             value={ attributes.content }
                             onChange={ content => setAttributes( { content } ) }
                             help={ __('Paste here the content of the Embed Code when you press the "Share" button on an EPFL tableau view', 'wp-gutenberg-epfl') }
                         />
-                    </PanelBody>
-                    
-                    <PanelBody title={ __('OR ', 'wp-gutenberg-epfl') }>
-                        <label>URL</label>
+                    <h4>OR</h4>
                         <TextControl
+							label={ __('URL', 'wp-gutenberg-epfl') }
                             value={ attributes.url }
                             onChange={ url => setAttributes( { url } ) }
                             help={ __('Url of the view (eg. "EPFLofficialstatistics/StatistiquesOfficielles")', 'wp-gutenberg-epfl') }
                         />
-                        <label>Width</label>
-                        <TextControl
-                            value={ attributes.width }
-                            onChange={ width => setAttributes( { width } ) }
-                        />
-                        <label>Height</label>
-                        <TextControl
-                            value={ attributes.height }
-                            onChange={ height => setAttributes( { height } ) }
-                        />
-                    </PanelBody>
-                </InspectorControls>
-                <div className={ className }>
-                    <div id="preview-box">
-                        <h2>EPFL TABLEAU</h2>
-                        <div className="helper">{ __('Please fill the fields in the right-hand column', 'wp-gutenberg-epfl') }</div>
-                    </div>
                 </div>
             </Fragment>
-		)		
+		)
 	},
 	save: ( props ) => {
 		return null;
