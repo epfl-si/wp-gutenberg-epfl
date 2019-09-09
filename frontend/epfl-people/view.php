2c2a0229ed68a2cb2758407e9581e0a71a1062c5
LuluTchab <l.chaboudez@gmail.com>
2019-09-09 14:12:25 +0200

    Rollback to one func per action

--- frontend/epfl-people/view.php
+++ frontend/epfl-people/view.php
@@ -7,12 +7,19 @@
 
     function epfl_people_render($persons, $from, $columns) {
 
-        //var_dump($persons);
-        //var_dump($from);
-        //var_dump($columns);
+        //error_log("epfl_people_render persons: ".$persons);
+        //error_log("epfl_people_render from: ".$from);
+        //error_log("epfl_people_render columns: ".$columns);
 
-        $function_to_be_called = 'epfl_people_card';
-        $markup = $function_to_be_called($persons, $from, $columns);
+        $function_to_be_called = __NAMESPACE__.'\epfl_people_'.$columns;
+        $markup = $function_to_be_called($persons, $from);
         return $markup;
+    }
+
+    function epfl_people_1($persons, $from) {
+        return epfl_people_card($persons, $from, '1');
+    }
+    function epfl_people_3($persons, $from) {
+        return epfl_people_card($persons, $from, '3');
     }
 
