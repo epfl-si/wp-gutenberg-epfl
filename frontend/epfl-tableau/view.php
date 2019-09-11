448ccda158a5bd5b789960d33ef4816eef119ae0
LuluTchab <l.chaboudez@gmail.com>
2019-09-11 13:56:15 +0200

    Change attributes names, prepare for migration from shortcodes, change field label

--- frontend/epfl-tableau/view.php
+++ frontend/epfl-tableau/view.php
@@ -1,6 +1,6 @@
 <?php
 
-function epfl_tableau_render($url, $width, $height) {
+function epfl_tableau_render($tableau_name, $width, $height) {
 
     $markup = '<div class="container my-3">';
     $markup .= '<script type="text/javascript" src="https://tableau.epfl.ch/javascripts/api/viz_v1.js"></script>';
@@ -9,7 +9,7 @@ function epfl_tableau_render($url, $width, $height) {
     $markup .= '<param name="host_url" value="https%3A%2F%2Ftableau.epfl.ch%2F" />';
     $markup .= '<param name="embed_code_version" value="3" />';
     $markup .= '<param name="site_root" value="" />';
-    $markup .= '<param name="name" value="' . esc_attr($url) . '" />';
+    $markup .= '<param name="name" value="' . esc_attr($tableau_name) . '" />';
     $markup .= '<param name="tabs" value="no" />';
     $markup .= '<param name="toolbar" value="yes" />';
     $markup .= '<param name="showAppBanner" value="false" />';
