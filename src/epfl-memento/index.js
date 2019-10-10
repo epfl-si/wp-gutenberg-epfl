ca24e2bf8f9ecdc3cb486275ca806fb417010509
Grégoy Charmier <gregory.charmier@gmail.com>
2019-10-10 16:53:16 +0200

    update version

--- src/epfl-memento/index.js
+++ src/epfl-memento/index.js
@@ -13,7 +13,7 @@ registerBlockType(
 	'epfl/memento',
 	{
 		title: __('EPFL Memento', 'wp-gutenberg-epfl'),
-		description: 'v1.0.1',
+		description: 'v1.0.2',
 		icon: mementoIcon,
 		category: 'common',
 		keywords: [
