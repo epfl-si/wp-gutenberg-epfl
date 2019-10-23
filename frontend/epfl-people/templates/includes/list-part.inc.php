b98e74756322b1edd06e2efce0cdf491c69ba0cc
Grégoy Charmier <gregory.charmier@gmail.com>
2019-10-23 14:40:04 +0200

    Fix visual prob

--- frontend/epfl-people/templates/includes/list-part.inc.php
+++ frontend/epfl-people/templates/includes/list-part.inc.php
@@ -11,14 +11,22 @@ if ($photo_url) {
 
 $markup .= '</div>';
 $markup .= '<a href="' . esc_url($people_url) . '" class="contact-list-item" itemprop="name">' . esc_attr($person->prenom) . ' ' . esc_attr($person->nom) . '</a>';
-$markup .= '<p class="contact-list-item m-0 text-muted" itemprop="jobTitle">' . esc_html($function) . '</p>';
-$markup .= '<a class="contact-list-item text-muted" href="mailto:"' . esc_attr($person->email) . '" itemprop="email">' . esc_attr($person->email) . "</a>";
 
-if (isset($phones[0])) { 
-    $markup .= '<a class="contact-list-item text-muted" href="tel:"'. esc_html($phones[0]) . '" itemprop="telephone">';
-    $markup .= '<b>' . esc_html($phones[0]) . '</b>';
+if ($function) {
+    $markup .= '<p class="contact-list-item m-0 text-muted" itemprop="jobTitle">' . esc_html($function) . '</p>';
+} else {
+    $markup .= '<div class="contact-list-item"></div>';
 }
 
-$markup .= '</a>';
+$markup .= '<a class="contact-list-item text-muted" href="mailto:"' . esc_attr($person->email) . '" itemprop="email">' . esc_attr($person->email) . "</a>";
+
+if (isset($phones[0])) {
+    $markup .= '<a class="contact-list-item text-muted" href="tel:"'. esc_html($phones[0]) . '" itemprop="telephone">';
+    $markup .= '<b>' . esc_html($phones[0]) . '</b>';
+    $markup .= '</a>';
+} else {
+    $markup .= '<div class="contact-list-item"></div>';
+}
+
 $markup .= '<a class="contact-list-item text-muted" href="' . esc_url($room_url) . '" itemprop="workLocation">' . esc_html($room) . '</a>';
 $markup .= '</div>';
