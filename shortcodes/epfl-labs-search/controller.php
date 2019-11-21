28037221bcc0491453751a5f31e4c63431076c72
Julien Delasoie <julien.delasoie@epfl.ch>
2019-11-21 14:37:25 +0100

    Allow to filter by tag a list of labs

--- shortcodes/epfl-labs-search/controller.php
+++ shortcodes/epfl-labs-search/controller.php
@@ -5,16 +5,39 @@ namespace EPFL\Plugins\Shortcodes\EPFLLabSearch;
 /**
  * render the shortcode, mainly a form and his table
  */
-function renderLabsSearch($sites, $faculty, $institute) {
+function renderLabsSearch($sites, $faculty, $institute, $field) {
   wp_enqueue_script( 'lib-listjs', plugins_url('lib/list.min.js', dirname(__FILE__)), ['jquery'], 1.5, false);
   wp_enqueue_style( 'epfl-labs-search-css', plugins_url('epfl-labs-search.css', __FILE__),false,'1.1','all');
 
   filter_out_unused_language($sites);
 
+  // are we doing a field of research result ?
+  if ( !empty($field)) {
+    // Remove all laboratories that are not in the current field of research
+    foreach ($sites as $key => $site) {
+      $tags_in_site = [];
+      foreach ($site->tags as $tag) {
+        $tags_in_site[] = $tag->name;
+      }
+      if (!in_array($field, $tags_in_site)) {
+        unset($sites[$key]);
+      }
+    }
+  }
+  $combo_list_content = separate_tags_by_type($sites);
+  if ( empty($sites) ) {
+    // Disallow unknown text in the field var, as it will be shown in first page
+    // that's a bad field of research, reset is but keep the empty list
+    $field = "";
+  } else {
+    set_query_var('epfl_labs-predefined_field', $field);
+  }
+
   set_query_var('epfl_labs-sites', $sites);
   set_query_var('epfl_labs-predefined_faculty', $faculty);
   set_query_var('epfl_labs-predefined_institute', $institute);
-  set_query_var('eplf_labs-combo_list_content', separate_tags_by_type($sites));
+  set_query_var('eplf_labs-combo_list_content', $combo_list_content);
+
   load_template(dirname(__FILE__).'/view.php');
 }
 
@@ -50,9 +73,8 @@ function filter_out_unused_language($sites) {
 function separate_tags_by_type($sites) {
   $tags_typped = [
     'faculty' => [],
-    'institute' => []
-    /*'field-of-research' => []
-    */
+    'institute' => [],
+    'field-of-research' => []
   ];
 
   $current_language = get_current_language();
@@ -74,7 +96,6 @@ function separate_tags_by_type($sites) {
       }
     }
   }
-
 
   # sort everything
   foreach ($tags_typped as $key=>$tag_type) {
