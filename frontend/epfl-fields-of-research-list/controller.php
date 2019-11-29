1da0d7884792b14bf004f1c9f8ee635749f8f8dd
Julien Delasoie <julien.delasoie@epfl.ch>
2019-11-29 14:13:46 +0100

    Set test url for demo

--- frontend/epfl-fields-of-research-list/controller.php
+++ frontend/epfl-fields-of-research-list/controller.php
@@ -1,16 +1,21 @@
 <?php
 namespace EPFL\Plugins\Gutenberg\FieldsOfResearchList;
+
 require_once(dirname(__FILE__).'/../lib/utils.php');
 require_once(dirname(__FILE__).'/../lib/language.php');
 require_once(dirname(__FILE__).'/controller.php');
+
 use \EPFL\Plugins\Gutenberg\Lib\Utils;
 use function EPFL\Plugins\Gutenberg\Lib\Language\get_current_or_default_language;
-define(__NAMESPACE__ . "\LABS_INFO_PROVIDER_URL", "https://wp-veritas.epfl.ch/api/v1/");
+
+#define(__NAMESPACE__ . "\LABS_INFO_PROVIDER_URL", "https://wp-veritas.epfl.ch/api/v1/");
+define(__NAMESPACE__ . "\LABS_INFO_PROVIDER_URL", "https://wp-veritas.128.178.222.83.nip.io/api/v1/");
+
 function epfl_fields_of_research_list_block($attributes) {
     # by default get all sites with at least a tag
     $lang = get_current_or_default_language();
     $url = LABS_INFO_PROVIDER_URL . 'tags/?type=field-of-research';
-    $fields = Utils::get_items($url);
+    $fields = Utils::get_items($url, 300, 5, false);
     ob_start();
 ?>
 <div class="container-full">
