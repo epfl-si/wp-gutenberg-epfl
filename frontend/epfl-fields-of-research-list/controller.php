4cc4e2a46b5b3adcb09101226302ddd9e1388ac7
Julien Delasoie <julien.delasoie@epfl.ch>
2019-12-02 10:17:58 +0100

    Filter out unused langage on data

--- frontend/epfl-fields-of-research-list/controller.php
+++ frontend/epfl-fields-of-research-list/controller.php
@@ -11,23 +11,51 @@ use function EPFL\Plugins\Gutenberg\Lib\Language\get_current_or_default_language
 #define(__NAMESPACE__ . "\LABS_INFO_PROVIDER_URL", "https://wp-veritas.epfl.ch/api/v1/");
 define(__NAMESPACE__ . "\LABS_INFO_PROVIDER_URL", "https://wp-veritas.128.178.222.83.nip.io/api/v1/");
 
+
+/**
+ * Simplify the sites data by removing and renaming languages fields
+ */
+function filter_out_unused_language($fields) {
+  $current_language = get_current_language();
+  $filtered_fields = [];
+
+  foreach ($fields as $field) {
+
+    $object = new \stdClass();
+    if ($current_language === 'fr') {
+      $object->name = $field->name_en;
+      $object->url = $field->url_en;
+    } else {
+      $object->name = $field->name_fr;
+      $object->url = $field->url_fr;
+    }
+    $filtered_fields[] = $object;
+  }
+  return $filtered_fields;
+}
+
 function epfl_fields_of_research_list_block($attributes) {
     # by default get all sites with at least a tag
     $lang = get_current_or_default_language();
     $url = LABS_INFO_PROVIDER_URL . 'tags/?type=field-of-research';
-    #$fields = Utils::get_items($url);
+    # $fields = Utils::get_items($url);
     $fields = Utils::get_items($url, 300, 5, false);
+
+    $fields = filter_out_unused_language($fields);
+    # sort alpha
+    usort($fields, function($a, $b) {
+      return strcasecmp($a->name, $b->name);
+    });
+
     ob_start();
 ?>
 <div class="container-full">
   <div class="container">
     <ul class="list-group list-group-flush">
       <?php foreach($fields as $field): ?>
-        <?php $name = ($lang == 'fr')?$field->name_fr:$field->name_en; ?>
-        <?php $url  = ($lang == 'fr')?$field->url_fr:$field->url_en; ?>
       <li class="list-group-item">
-          <a href="<?php echo esc_attr($url); ?>">
-            <?php echo esc_html($name); ?>
+          <a href="<?php echo esc_attr($field->url); ?>">
+            <?php echo esc_html($field->name); ?>
           </a>
       </li>
       <?php endforeach; ?>
