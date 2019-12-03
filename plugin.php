6f0321dc00de4cdef0d9859e33ab543e39469091
Grégoy Charmier <gregory.charmier@gmail.com>
2019-12-03 10:00:30 +0100

    update version number

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: wp-gutenberg-epfl
  * Description: EPFL Gutenberg Blocks
  * Author: WordPress EPFL Team
- * Version: 1.2.5
+ * Version: 1.3.4
  */
 
 namespace EPFL\Plugins\Gutenberg;
