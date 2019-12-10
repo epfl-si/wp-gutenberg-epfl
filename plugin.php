c4efad56930ac317a7306bfd082a1982429c3f77
LuluTchab <l.chaboudez@gmail.com>
2019-12-10 11:07:46 +0100

    Aspirateur

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: wp-gutenberg-epfl
  * Description: EPFL Gutenberg Blocks
  * Author: WordPress EPFL Team
- * Version: 1.5.8
+ * Version: 1.5.9
  */
 
 namespace EPFL\Plugins\Gutenberg;
