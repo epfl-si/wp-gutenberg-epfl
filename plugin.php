cd574ea4d220d6ec3d154f7ecba94429072e30f2
LuluTchab <l.chaboudez@gmail.com>
2019-12-13 11:23:07 +0100

    Update plugin version

--- plugin.php
+++ plugin.php
@@ -3,7 +3,7 @@
  * Plugin Name: wp-gutenberg-epfl
  * Description: EPFL Gutenberg Blocks
  * Author: WordPress EPFL Team
- * Version: 1.5.12
+ * Version: 1.5.13
  */
 
 namespace EPFL\Plugins\Gutenberg;
