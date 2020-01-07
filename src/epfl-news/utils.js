c7672afc91dfccd8ff128486d9d1b1be50f18c8b
Grégoy Charmier <gregory.charmier@gmail.com>
2020-01-07 10:09:58 +0100

    rename BASE_API_REST_URL to BASE_NEWS_API_REST_URL

--- src/epfl-news/utils.js
+++ src/epfl-news/utils.js
@@ -1,1 +1,6 @@
-global.BASE_API_REST_URL = "https://staging-actu.epfl.ch/api/v1/";
+const prod = true;
+if (prod) {
+  global.BASE_NEWS_API_REST_URL = "https://actu.epfl.ch/api/v1/";
+} else {
+  global.BASE_NEWS_API_REST_URL = "https://staging-actu.epfl.ch/api/v1/";
+}
