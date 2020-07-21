8ab97b6c8a4aa358e47e883071ec8208b39f20cd
LuluTchab <l.chaboudez@gmail.com>
2020-07-21 09:35:14 +0200

    PDF Flipbook block

---
+++ frontend/epfl-pdf-flipbook/view.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace EPFL\Plugins\Gutenberg\PDFFlipbook;
+use \EPFL\Plugins\Gutenberg\Lib\Utils;
+
+require_once(dirname(__FILE__).'/../lib/utils.php');
+
+function epfl_pdf_flipbook_block( $attributes ) {
+
+    $pdf_id    = Utils::get_sanitized_attribute( $attributes, 'pdfId' );
+
+
+    $code = '<div class="container">'.
+            '[flipbook pdf="'.wp_get_attachment_url($pdf_id).'" same_height_as="body" theme="light"]'.
+            '</div>';
+    return do_shortcode($code);
+}
