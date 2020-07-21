abf1a61800311b20c528a836a6826939ad76dde5
LuluTchab <l.chaboudez@gmail.com>
2020-07-21 15:06:16 +0200

    Use Toggle and display several fields

--- frontend/epfl-semester-projects/controller.php
+++ frontend/epfl-semester-projects/controller.php
@@ -21,20 +21,41 @@ function epfl_semester_projects_block($attributes, $inner_content) {
     $items = Utils::get_items($url, 0, 5, false);
 
 
-    $html = '<div class="container mb-4">'.
-    '<div class="links-group">'.
-        '<h5 id="links-group-title">'.$title.'</h5>'.
-        '<nav class="nav flex-column flex-wrap align-items-start" role="navigation" aria-labelledby="links-group-title">';
-
     foreach($items as $item)
     {
+      $attributes = array('title' => $item->project->title);
 
-      $html .= '<a class="nav-link nav-pretty" href="'.'#'.'">'.$item->project->title.'</a>';
+      $types = array();
+      foreach($item->project->types as $type){ $types[] = $type->fr;}
+
+      $content_array = array('No project'   => $item->project->noProjet->fr,
+                            'Image'         => $item->project->image->fr,
+                            'Types'         => implode(", ", $types),
+                            'Section'       => $item->project->section->fr,
+                            'Principal 1'   => $item->project->enseignants->principal1->name->fr,
+                            'Rapport'       => $item->project->rapport->fr,
+                            'Présentation'  => $item->project->presentation->fr,
+                            'Archivage'     => $item->project->archivage->fr,
+                            'Descriptif'    => $item->project->descriptif->fr,
+                            'Commentaires'  => $item->project->commentaire->fr,
+                            'Professors'    => $item->project->enseignants->principal1->name->fr.", ".
+                                                $item->project->enseignants->principal2->name->fr,
+                            'Resp. Admin'   => $item->project->administrateur->fr,
+                            'Site'          => $item->project->site->fr,
+                            'Labo, site, mail' => $item->project->externe->laboratoire->fr. ", ".
+                                                  $item->project->externe->site->fr." ,".
+                                                  $item->project->externe->email->fr
+                          );
+
+      $inner_content = array();
+      foreach($content_array as $title => $value)
+      {
+        $inner_content[] = "<b>".$title.": </b>".$value;
+      }
+
+      $html .= \EPFL\Plugins\Gutenberg\Toggle\epfl_toggle_block( $attributes, implode("<br>", $inner_content) );
     }
 
-
-    $html .= '</nav>'.
-    '</div></div>';
 
     return $html;
   }
