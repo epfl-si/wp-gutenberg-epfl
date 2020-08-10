d9e4b2db0bd1ebe370b4f92d70edab31a39701b9
LuluTchab <l.chaboudez@gmail.com>
2020-08-10 10:14:08 +0200

    Final render

--- frontend/epfl-carousel/controller.php
+++ frontend/epfl-carousel/controller.php
@@ -8,7 +8,9 @@ require_once(dirname(__FILE__).'/../lib/utils.php');
 function epfl_carousel_block( $attributes ) {
     $elementCount = 0;
 
-    for($i = 1; $i <= 3; $i++){
+    $MAX_ELEMENTS = 5;
+
+    for($i = 1; $i <= $MAX_ELEMENTS; $i++){
         # sanitize and count titles first
         $attributes['title'.$i]       = Utils::get_sanitized_attribute( $attributes, 'title'.$i );
 
@@ -18,66 +20,107 @@ function epfl_carousel_block( $attributes ) {
 
         $attributes['imageId'.$i]       = Utils::get_sanitized_attribute( $attributes, 'imageId'.$i );
         $attributes['url'.$i]           = Utils::get_sanitized_attribute( $attributes, 'url'.$i );
-        $attributes['buttonLabel'.$i]   = Utils::get_sanitized_attribute( $attributes, 'buttonLabel'.$i );
-        $attributes['excerpt'.$i]       = Utils::get_sanitized_attribute( $attributes, 'excerpt'.$i );
+        $attributes['description'.$i]   = Utils::get_sanitized_attribute( $attributes, 'description'.$i );
         
     }
 
-    $attributes['grayBackground'] = Utils::get_sanitized_attribute( $attributes, 'grayBackground', False);
-    $greyClasses = '';
-    if ($attributes['grayBackground']) {
-      $greyClasses = 'bg-gray-100 py-4 mt-4';
-    }
-
-    $open_links_new_tab = Utils::get_sanitized_attribute( $attributes, 'openLinksNewTab', False);
-
+    $open_links_new_tab = Utils::get_sanitized_attribute( $attributes, 'openLinksNewTab', '') != '';
+    
     ob_start();
 ?>
 
-<div class="container-full p-lg-5 <?php echo $greyClasses ?>">
-  <div class="container">
-    <?php if ($attributes['titleSection']): ?>
-    <h3 class="h6 mb-3<?php echo ($elementCount < 3) ? ' text-center' : '' ?>"><?php echo $attributes['titleSection'] ?></h3>
-    <?php endif; ?>
-    <div class="card-deck<?php echo ($elementCount < 3) ? ' card-deck-line' : '' ?>">
-      <?php
-      for($i = 1; $i <= 3; $i++):
+<div class="container">
+<div id="carouselNews" class="carousel slide carousel-highlighted-news" data-ride="carousel" data-interval="6000">
+  <div class="carousel-inner">
+    
+
+    <?php
+      for($i = 1; $i <= $MAX_ELEMENTS; $i++):
         if ($attributes['title'.$i]) :
         $image = get_post($attributes['imageId'.$i]);
       ?>
-      <div class="card">
-      <?php if ($attributes['imageId'.$i]): ?>
-        <a <?php if($open_links_new_tab): ?>target="_blank" rel="noopener"<?php endif; ?> href="<?php echo $attributes['url'.$i] ?: '#' ?>" class="card-img-top">
-          <picture>
-          <?php echo wp_get_attachment_image(
+      <div class="carousel-item <?php if($i==1) echo 'active'; ?>">
+      <div class="fullwidth-teaser fullwidth-teaser-horizontal">
+        <picture>
+        <?php echo wp_get_attachment_image(
             $attributes['imageId'.$i],
-            'thumbnail_16_9_crop', // see functions.php
+            'thumbnail_16_9_large_80p', // see functions.php
             '',
             [
               'class' => 'img-fluid',
               'title' => $image->post_excerpt
             ]
             ) ?>
-          </picture>
-        </a>
-        <?php endif; ?>
-        <div class="card-body">
-          <h3 class="card-title">
-            <a <?php if($open_links_new_tab): ?>target="_blank" rel="noopener"<?php endif; ?> href="<?php echo $attributes['url'.$i] ?: '#' ?>"><?php echo $attributes['title'.$i] ?: __('Title', 'epfl') ?></a>
-          </h3>
-          <p><?php echo $attributes['excerpt'.$i] ?></p>
-        </div>
-        <div class="card-footer mt-auto">
-          <a <?php if($open_links_new_tab): ?>target="_blank" rel="noopener"<?php endif; ?> href="<?php echo $attributes['url'.$i] ?: '#' ?>" class="btn btn-secondary btn-sm"><?php echo $attributes['buttonLabel'.$i] ?: __('See more', 'epfl') ?></a>
+        </picture>
+
+        <div class="fullwidth-teaser-text">
+
+          <div class="fullwidth-teaser-header">
+            <div class="fullwidth-teaser-title">
+              <h3>
+              <?php echo $attributes['title'.$i] ?>
+              </h3>
+            </div>
+            <a href="<?php echo esc_url($attributes['url'.$i]); ?>" <?php if($open_links_new_tab) echo 'target="_blank"'; ?>  aria-label="Link to read more of that page" class="btn btn-primary triangle-outer-top-right d-none d-xl-block">
+            <?php echo __("Read more", 'epfl'); ?>
+              <svg class="icon" aria-hidden="true">
+                <use xlink:href="#icon-chevron-right"></use>
+              </svg>
+            </a>
+          </div>
+
+          <div class="fullwidth-teaser-content">
+            <p>
+              <?php echo $attributes['description'.$i] ?>
+            </p>
+          </div>
+
+          <div class="fullwidth-teaser-footer">
+            <a href="#" aria-label="Link to read more of that page" class="btn btn-primary btn-block d-xl-none"><?php echo __("Read more", 'epfl'); ?></a>
+          </div>
         </div>
       </div>
-      <?php
-        endif;
-        endfor;
-      ?>
     </div>
+    <?php
+      endif;
+      endfor;
+    ?>
   </div>
+
+  <ol class="carousel-indicators">
+  <?php 
+  $slide_index = 0;
+  for($i = 1; $i <= $MAX_ELEMENTS; $i++)
+  {
+    
+    if ($attributes['title'.$i])
+    {
+      $class = ($i==1)?'class="active"':'';
+
+      echo '<li data-target="#carouselNews" data-slide-to="'.($slide_index).'" '.$class.'></li>';
+
+      $slide_index++;
+    }
+  }
+  ?>
+  </ol>
+
+  <button role="button" class="carousel-control carousel-control-prev" role="button" data-slide="prev" data-target="#carouselNews">
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-chevron-left"></use>
+    </svg>
+    <span class="sr-only">Previous</span>
+  </button>
+  <button role="button" class="carousel-control carousel-control-next" role="button" data-slide="next" data-target="#carouselNews">
+    <svg class="icon" aria-hidden="true">
+      <use xlink:href="#icon-chevron-right"></use>
+    </svg>
+    <span class="sr-only">Next</span>
+  </button>
 </div>
+</div>
+
+
 
 <?php
     $content = ob_get_contents();
