e0ea5782abc3747b48a0b0bcb2670e4b8dab54ea
Julien Delasoie <julien.delasoie@epfl.ch>
2020-08-31 14:37:04 +0200

    Update to latest build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-polyfill'), 'version' => 'd568312c48d95490868384cf0d168fb8');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-polyfill'), 'version' => '10ef7c92233d0b348c9a8e5b441f28b6');
