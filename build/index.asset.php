c170c7c2e81db4e1b7417eff72b497ea6d4ded74
LuluTchab <l.chaboudez@gmail.com>
2020-09-07 17:10:14 +0200

    Adding build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-polyfill'), 'version' => 'b3ed3cb92247d74b095d2fc776552b54');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-polyfill'), 'version' => 'e67e4d9efff0ed49926dfcc50a09072b');
