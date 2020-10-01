30d43cf0f597b136c97179a32131504d5ba09309
Julien Delasoie <julien.delasoie@epfl.ch>
2020-10-01 10:32:10 +0200

    Fix missing $ as jQuery definition

--- frontend/epfl-faq/js/faq-header.js
+++ frontend/epfl-faq/js/faq-header.js
@@ -1,11 +1,11 @@
 // Looping through faq blocks
-$(".epfl-faq-header").each(function(header_ul_index, header_ul) {
+jQuery(".epfl-faq-header").each(function(header_ul_index, header_ul) {
 
     // Looping through questions inside current faq block
-    $(header_ul).parent().find('.faq-item-question').each(function(question_item_index, question_item) {
+    jQuery(header_ul).parent().find('.faq-item-question').each(function(question_item_index, question_item) {
 
         // Adding link in header
-        $(header_ul).append('<li><a href="#'+$(question_item).parent().attr('id')+'">'+$(question_item).html()+'</a></li>');
+        jQuery(header_ul).append('<li><a href="#'+jQuery(question_item).parent().attr('id')+'">'+jQuery(question_item).html()+'</a></li>');
     });
 
 });
