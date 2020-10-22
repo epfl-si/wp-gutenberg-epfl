928296849a456ea0e49e51b8847004403ba6879f
Julien Delasoie <julien.delasoie@epfl.ch>
2020-10-22 09:30:20 +0200

    Prototype the alert block

--- src/epfl-alert/index.js
+++ src/epfl-alert/index.js
@@ -1,118 +1,134 @@
 import {
-	hasCommonCategory,
-	getTooltippedAttributes,
-	getTooltippedExample,
+  hasCommonCategory,
+  getTooltippedAttributes,
+  getTooltippedExample,
 } from '../block-utils.js'
+
+const classNames = require('classnames')
 
 import alertIcon from './alert-icon'
 
 const version = "v1.0.0";
 
-const { __ } = wp.i18n;
+const {__} = wp.i18n;
 
 const {
-	registerBlockType,
+  registerBlockType,
 } = wp.blocks;
 
 const {
-    InspectorControls,
-    RichText,
+  InspectorControls,
+  RichText,
 } = wp.blockEditor;
 
 const {
-    PanelBody,
-	SelectControl,
-    ToggleControl,
+  PanelBody,
+  SelectControl,
+  ToggleControl,
 } = wp.components;
 
-const { Fragment } = wp.element;
+const {Fragment} = wp.element;
 
 let optionsAlertType = [
-    { value: 'info', label: __('Info', 'epfl') },
-    { value: 'success', label: __('Success', 'epfl') },
-    { value: 'warning', label: __('Warning', 'epfl') },
-    { value: 'danger', label: __('Danger', 'epfl') },
+  {value: 'info', label: __('Info', 'epfl')},
+  {value: 'success', label: __('Success', 'epfl')},
+  {value: 'warning', label: __('Warning', 'epfl')},
+  {value: 'danger', label: __('Danger', 'epfl')},
 ];
 
-registerBlockType( 'epfl/alert', {
-	title: __( 'EPFL Alert', 'epfl'),
-	description: __(
-		'Show a message inside a notification zone',
-		'epfl'
-	),
-	icon: alertIcon,
-	category: hasCommonCategory ? 'common' : 'widgets',
-	attributes: getTooltippedAttributes({
-        content: {
-			type: 'string',
-		},
-		largeDisplay: {
-            type: 'boolean',
-            default: false,
-        },
-        canBeClosed: {
-            type: 'boolean',
-            default: false,
-        },
-        alertType: {
-            type: 'string',
-            default: 'info',
-        }
-	}),
-	example: getTooltippedExample(),
-	supports : {
-		customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
-	},
-	edit: ( props ) => {
-        const { attributes, className, setAttributes } = props
+registerBlockType('epfl/alert', {
+  title: __('EPFL Alert', 'epfl'),
+  description: __(
+    'Show a message inside a notification zone',
+    'epfl'
+  ),
+  icon: alertIcon,
+  category: hasCommonCategory ? 'common' : 'widgets',
+  attributes: getTooltippedAttributes({
+    content: {
+      type: 'string',
+    },
+    largeDisplay: {
+      type: 'boolean',
+      default: false,
+    },
+    canBeClosed: {
+      type: 'boolean',
+      default: false,
+    },
+    alertType: {
+      type: 'string',
+      default: 'info',
+    }
+  }),
+  example: getTooltippedExample(),
+  supports: {
+    customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
+  },
+  edit: (props) => {
+    const {attributes, className, setAttributes} = props
 
-		if ( attributes.asToolTip ) {
-			// render the tooltip
-			return (
-				<Fragment>
-					<img src={ blockThumbnails.alert } />
-				</Fragment>
-			);
-		}
+    if (attributes.asToolTip) {
+      // render the tooltip
+      return (
+        <Fragment>
+          <img src={blockThumbnails.alert}/>
+        </Fragment>
+      );
+    }
 
-        return (
-            <Fragment>
-				<InspectorControls>
-					<p><a className="wp-block-help" href={ __('https://www.epfl.ch/campus/services/website/alert-en/', 'epfl') } target="new">{ __('Online help', 'epfl') } </a></p>
-					<p className="wp-block-help">{ version }</p>
-					<PanelBody title='Format'>
-                        <ToggleControl
-                            label={ __('Large display', 'epfl') }
-                            checked={ attributes.largeDisplay }
-                            onChange={ largeDisplay => setAttributes( { largeDisplay } ) }
-                        />
-                        <ToggleControl
-                            label={ __('Alert can be closed', 'epfl') }
-                            checked={ attributes.canBeClosed }
-                            onChange={ canBeClosed => setAttributes( { canBeClosed } ) }
-                        />
-                        <SelectControl
-							label={ <h4> { __('Alert type', 'epfl') } </h4> }
-							value={ attributes.alertType }
-							onChange={ alertType => setAttributes( { alertType } ) }
-							options={ optionsAlertType }
-						/>
-					</PanelBody>
-				</InspectorControls>
-                <div className={ className }>
-					<h2 className="epfl-block-title">{ __('EPFL Alert' , 'epfl') }</h2>
-					<label><small>{ __('Content', 'epfl') }</small></label>
-                    <RichText
-						value={ attributes.content }
-						onChange={ content => setAttributes( { content } ) }
-						placeholder={ __('Write your text here','epfl')}
-						keepPlaceholderOnFocus = { true }
-                    />
-					</div>
-            </Fragment>
-		)
-	},
-	save: ( props ) => {
-		return null;
-	},
-} );
+    return (
+      <Fragment>
+        <InspectorControls>
+          <p><a className="wp-block-help" href={__('https://www.epfl.ch/campus/services/website/alert-en/', 'epfl')}
+                target="new">{__('Online help', 'epfl')} </a></p>
+          <p className="wp-block-help">{version}</p>
+          <PanelBody title='Format'>
+            <ToggleControl
+              label={__('Large display', 'epfl')}
+              checked={attributes.largeDisplay}
+              onChange={largeDisplay => setAttributes({largeDisplay})}
+            />
+            <ToggleControl
+              label={__('Alert can be closed', 'epfl')}
+              checked={attributes.canBeClosed}
+              onChange={canBeClosed => setAttributes({canBeClosed})}
+            />
+            <SelectControl
+              label={__('Alert type', 'epfl')}
+              value={attributes.alertType}
+              onChange={alertType => setAttributes({alertType})}
+              options={optionsAlertType}
+            />
+          </PanelBody>
+        </InspectorControls>
+        <div className={classNames({'container': attributes.largeDisplay})}>
+          <div
+            className={
+              classNames('alert',
+              `alert-${attributes.alertType}`,
+              {container: attributes.largeDisplay},
+              {'alert-dismissible fade show': attributes.canBeClosed}
+            )}
+            role='alert'
+          >
+            <RichText
+              value={ attributes.content}
+              onChange={content => setAttributes({content})}
+              placeholder={__('Write your text here', 'epfl')}
+              keepPlaceholderOnFocus={true}
+            />
+            { attributes.canBeClosed && (
+              <button type='button' className='close' data-dismiss='alert' aria-label='Close'>
+                <span aria-hidden='true'>&times;</span>
+              </button>
+            )}
+          </div>
+        </div>
+      </Fragment>
+    )
+  },
+  save: (props) => {
+    return null;
+  },
+});
