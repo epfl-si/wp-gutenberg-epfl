234e1067ef419da651046d13fdea4f73fbc2c43c
GregLeBarbar <GregLeBarbar@users.noreply.github.com>
2020-10-28 10:18:49 +0000

    Commit built files from master

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'f166f0fcd40e764d63c65a0413fc6211');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => 'a08d60bdc58d41537a65de956ce17188');
