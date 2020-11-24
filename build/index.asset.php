03ba5a33d50c6f3f94bf005773b31166f4a040c8
jdelasoie <julien.delasoie@epfl.ch>
2020-11-24 08:02:40 +0000

    Commit built files from master

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '79dc19908dc4bec3aa774f58be013199');
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-components', 'wp-element', 'wp-polyfill', 'wp-primitives'), 'version' => '96cc28907f7580a68f5267ce2623abbf');
