c70475cb4779dfe47c9bba086a7a4fbd4c2ba82a
Julien Delasoie <julien.delasoie@epfl.ch>
2021-03-10 11:11:49 +0100

    Change server name for stating actu

--- src/epfl-news/utils.js
+++ src/epfl-news/utils.js
@@ -2,5 +2,5 @@ const prod = true;
 if (prod) {
   global.BASE_NEWS_API_REST_URL = "https://actu.epfl.ch/api/v1/";
 } else {
-  global.BASE_NEWS_API_REST_URL = "https://staging-actu.epfl.ch/api/v1/";
+  global.BASE_NEWS_API_REST_URL = "https://stage-actu.epfl.ch/api/v1/";
 }
