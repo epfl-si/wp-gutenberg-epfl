84e206729e142f30850b6329329f991e2c479efd
Grégory Charmier <gregory.charmier@gmail.com>
2021-06-17 16:30:39 +0200

    [version] update version numbers

--- src/epfl-people/index.js
+++ src/epfl-people/index.js
@@ -8,7 +8,7 @@ import newsIcon from './people-icon'
 import PreviewPeople from './preview'
 import InspectorControlsPeople from './inspector'
 
-export const version = "v1.1.11";
+export const version = "v1.2.0";
 
 const {__} = wp.i18n
 const {registerBlockType} = wp.blocks
