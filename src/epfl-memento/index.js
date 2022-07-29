129f4f17f6074e9b9038f9feb56e9134d3fb3052
Julien Delasoie <julien.delasoie@epfl.ch>
2022-07-29 14:41:17 +0200

    Running auto-format on all the memento JS code

--- src/epfl-memento/index.js
+++ src/epfl-memento/index.js
@@ -1,108 +1,108 @@
 import {
-    hasCommonCategory,
-    getTooltippedAttributes,
-    getTooltippedExample,
+	hasCommonCategory,
+	getTooltippedAttributes,
+	getTooltippedExample,
 } from '../block-utils.js'
 
-import * as axios from 'axios';
+import * as axios from 'axios'
 
 import './style.scss'
 
 import mementoIcon from './memento-icon'
 import PreviewMemento from './preview'
 import InspectorControlsMemento from './inspector'
-import './utils.js';
+import './utils.js'
 
-export const version = "v1.4.0";
+export const version = 'v1.4.0'
 
-const {__} = wp.i18n
-const {registerBlockType} = wp.blocks
-const {Fragment} = wp.element
+const { __ } = wp.i18n
+const { registerBlockType } = wp.blocks
+const { Fragment } = wp.element
 
 registerBlockType(
-    'epfl/memento',
-    {
-        title: __('EPFL Memento', 'epfl'),
-        description: __(
-            'Display events from memento.epfl.ch',
-            'epfl'
-        ),
-        icon: mementoIcon,
-        category: hasCommonCategory ? 'common' : 'embed',
-        keywords: [
-            __('events', 'epfl'),
-        ],
-        attributes: getTooltippedAttributes({
-            memento: {
-                type: 'integer',
-                default: 1,
-            },
-            template: {
-                type: 'string',
-                default: 'slider_with_the_first_highlighted_event',
-            },
-            nbEvents: {
-                type: 'integer',
-                default: 10,
-            },
-            lang: {
-                type: 'string',
-                default: 'en',
-            },
-            category: {
-              type: 'integer',
-              default: 0,
-            },
-            categories: {
-                type: 'string',
-                default: null,
-            },
-            period: {
-                type: 'string',
-                default: 'upcoming',
-            },
-            year: {
-                type: 'string',
-                default: 'no-filter',
-            },
-            keyword: {
-                type: 'string',
-                default: "",
-            }
-        }),
-        example: getTooltippedExample(),
-        supports: {
-            customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
-        },
+	'epfl/memento',
+	{
+		title: __('EPFL Memento', 'epfl'),
+		description: __(
+			'Display events from memento.epfl.ch',
+			'epfl'
+		),
+		icon: mementoIcon,
+		category: hasCommonCategory ? 'common' : 'embed',
+		keywords: [
+			__('events', 'epfl'),
+		],
+		attributes: getTooltippedAttributes({
+			memento: {
+				type: 'integer',
+				default: 1,
+			},
+			template: {
+				type: 'string',
+				default: 'slider_with_the_first_highlighted_event',
+			},
+			nbEvents: {
+				type: 'integer',
+				default: 10,
+			},
+			lang: {
+				type: 'string',
+				default: 'en',
+			},
+			category: {
+				type: 'integer',
+				default: 0,
+			},
+			categories: {
+				type: 'string',
+				default: null,
+			},
+			period: {
+				type: 'string',
+				default: 'upcoming',
+			},
+			year: {
+				type: 'string',
+				default: 'no-filter',
+			},
+			keyword: {
+				type: 'string',
+				default: '',
+			}
+		}),
+		example: getTooltippedExample(),
+		supports: {
+			customClassName: false, // Removes the default field in the inspector that allows you to assign a custom class
+		},
 
-        edit: props => {
-            const {attributes, className, setAttributes} = props
+		edit: props => {
+			const { attributes, className, setAttributes } = props
 
-            if ( attributes.asToolTip ) {
-                // render the tooltip
-                return(
-                    <Fragment>
-                        <img src={ blockThumbnails.memento } />
-                    </Fragment>
-                );
-            }
+			if (attributes.asToolTip) {
+				// render the tooltip
+				return (
+					<Fragment>
+						<img src={ blockThumbnails.memento }/>
+					</Fragment>
+				)
+			}
 
-            return (
-                <Fragment>
-                    <InspectorControlsMemento {...{attributes, setAttributes}} />
-                    <h2 className="epfl-block-title">{__('EPFL Memento', 'epfl')}</h2>
-                    <PreviewMemento {...{attributes, className}} />
-                </Fragment>
-            )
-        },
+			return (
+				<Fragment>
+					<InspectorControlsMemento { ...{ attributes, setAttributes } } />
+					<h2 className="epfl-block-title">{ __('EPFL Memento', 'epfl') }</h2>
+					<PreviewMemento { ...{ attributes, className } } />
+				</Fragment>
+			)
+		},
 
-        save: props => {
-            // This block is a dynamic block.
-            // So we save only something like this :
-            // <!-- wp:epfl/memento {"memento":"111","template":"4", ...}
-            // /-->
-            // The render of this block for the end user is doing in PHP.
-            return null
-        },
-    }
+		save: props => {
+			// This block is a dynamic block.
+			// So we save only something like this :
+			// <!-- wp:epfl/memento {"memento":"111","template":"4", ...}
+			// /-->
+			// The render of this block for the end user is doing in PHP.
+			return null
+		},
+	}
 )
--- src/epfl-memento/inspector.js
+++ src/epfl-memento/inspector.js
@@ -1,6 +1,6 @@
-import * as axios from 'axios';
-import React from 'react';
-import Select from 'react-select';
+import * as axios from 'axios'
+import React from 'react'
+import Select from 'react-select'
 import { version } from './index'
 
 const { __ } = wp.i18n
@@ -11,179 +11,193 @@ const {
 } = wp.blockEditor
 
 const {
-    PanelBody,
-    PanelRow,
-    SelectControl,
-    RadioControl,
-    TextControl,
-    RangeControl,
+	PanelBody,
+	PanelRow,
+	SelectControl,
+	RadioControl,
+	TextControl,
+	RangeControl,
 } = wp.components
 
 export default class InspectorControlsMemento extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            mementos: null,
-            categories: null,
-        }
-    }
-
-    componentDidMount() {
-        let apiRestUrl = BASE_MEMENTO_API_REST_URL;
-
-        let entryPointMementos = `${apiRestUrl}mementos/?format=json&limit=800&ordering=en_name`;
-		axios.get(entryPointMementos)
-			.then( response => response.data.results )
-			.then( mementos => this.setState({ mementos }) )
-            .catch( err => console.log(err))
-
-        let entryPointCategories = `${apiRestUrl}categories/?format=json&limit=800&ordering=en_label`;
-        axios.get(entryPointCategories)
-            .then( response => response.data.results )
-            .then( categories => this.setState({ categories }) )
-            .catch( err => console.log(err))
+	constructor(props) {
+		super(props)
+		this.state = {
+			mementos: null,
+			categories: null,
+		}
 	}
 
-    render() {
+	componentDidMount() {
+		let apiRestUrl = BASE_MEMENTO_API_REST_URL
 
-        const { attributes, setAttributes } = this.props
-        const handleCategoriesChange = ( categories ) => setAttributes( { categories: JSON.stringify( categories ) } );
+		let entryPointMementos = `${ apiRestUrl }mementos/?format=json&limit=800&ordering=en_name`
+		axios.get(entryPointMementos)
+			.then(response => response.data.results)
+			.then(mementos => this.setState({ mementos }))
+			.catch(err => console.log(err))
 
-        let content = "";
+		let entryPointCategories = `${ apiRestUrl }categories/?format=json&limit=800&ordering=en_label`
+		axios.get(entryPointCategories)
+			.then(response => response.data.results)
+			.then(categories => this.setState({ categories }))
+			.catch(err => console.log(err))
+	}
 
-        if (this.state.mementos !== null && this.state.categories !== null) {
+	render() {
 
-            let optionsMementosList = [];
+		const { attributes, setAttributes } = this.props
+		const handleCategoriesChange = (categories) => setAttributes({ categories: JSON.stringify(categories) })
 
-            this.state.mementos.forEach(memento => {
-                if (memento.is_active) {
-                  optionsMementosList.push({ label: memento.en_name, value: memento.id });
-                }
-            });
+		let content = ''
 
-            let optionsTemplatesList = [
-                { value: 'slider_with_the_first_highlighted_event', label: __('Template slider with the first highlighted event', 'epfl')},
-                { value: 'slider_without_the_first_highlighted_event', label: __('Template slider without the first highlighted event', 'epfl')},
-                { value: 'listing_with_the_first_highlighted_event', label: __('Template listing with the first highlighted event', 'epfl')},
-                { value: 'listing_without_the_first_highlighted_event', label: __('Template listing without the first highlighted event', 'epfl')},
-            ];
+		if (this.state.mementos !== null && this.state.categories !== null) {
 
-            let optionsLanguagesList = [
-                { value: 'fr', label: __('French', 'epfl') },
-                { value: 'en', label: __('English', 'epfl') },
-            ];
+			let optionsMementosList = []
 
-            let optionsPeriodsList = [
-                { value: 'upcoming', label: __('Upcomings events', 'epfl') },
-                { value: 'past', label: __('Past events', 'epfl') },
-            ];
+			this.state.mementos.forEach(memento => {
+				if (memento.is_active) {
+					optionsMementosList.push({ label: memento.en_name, value: memento.id })
+				}
+			})
 
-            let optionsCategoriesList = [
-                { value: 0, label: __('No filter', 'epfl') },
-            ];
+			let optionsTemplatesList = [
+				{
+					value: 'slider_with_the_first_highlighted_event',
+					label: __('Template slider with the first highlighted event', 'epfl')
+				},
+				{
+					value: 'slider_without_the_first_highlighted_event',
+					label: __('Template slider without the first highlighted event', 'epfl')
+				},
+				{
+					value: 'listing_with_the_first_highlighted_event',
+					label: __('Template listing with the first highlighted event', 'epfl')
+				},
+				{
+					value: 'listing_without_the_first_highlighted_event',
+					label: __('Template listing without the first highlighted event', 'epfl')
+				},
+			]
 
-            this.state.categories.forEach(category => {
-                if (category.activated) {
-                    optionsCategoriesList.push({ label: category.en_label, value: category.id });
-                }
-            });
+			let optionsLanguagesList = [
+				{ value: 'fr', label: __('French', 'epfl') },
+				{ value: 'en', label: __('English', 'epfl') },
+			]
 
-            let optionsYearsList = [
-              { value: 'no-filter', label: __('No filter', 'epfl') },
-            ]
+			let optionsPeriodsList = [
+				{ value: 'upcoming', label: __('Upcomings events', 'epfl') },
+				{ value: 'past', label: __('Past events', 'epfl') },
+			]
 
-            const currentYear = new Date().getFullYear();
-            for(let year = currentYear; year >= 2008 ; year--) {
-              optionsYearsList.push({ label: year, value: year });
-            }
+			let optionsCategoriesList = [
+				{ value: 0, label: __('No filter', 'epfl') },
+			]
 
-            let filterPastEventsByYear;
-            if (!!attributes.period && attributes.period === 'past') {
-                filterPastEventsByYear = (
-                    <SelectControl
-                        label={ __("Filter events by year", 'epfl') }
-                        value={ attributes.year }
-                        options={ optionsYearsList }
-                        onChange={ year => setAttributes( { year } ) }
-                    />
-                )
-            }
+			this.state.categories.forEach(category => {
+				if (category.activated) {
+					optionsCategoriesList.push({ label: category.en_label, value: category.id })
+				}
+			})
 
-            content = (
-                <InspectorControls>
-                    <p><a className="wp-block-help" href={ __('https://www.epfl.ch/campus/services/website/memento-en/', 'epfl') } target="new">{ __('Online help', 'epfl') } </a></p>
-                    <p className="wp-block-help">{ version }</p>
-                    <PanelBody title={ __('Memento', 'epfl') }>
-                        <SelectControl
-                            label={ __("Select your memento", 'epfl') }
-                            help={ __("The events come from the application memento.epfl.ch. If you don't have a memento, please send a request to 1234@epfl.ch", 'epfl') }
-                            value={ attributes.memento }
-                            options={ optionsMementosList }
-                            onChange={ memento => {
-                              memento = Number(memento);
-                              setAttributes( { memento } );
-                            }}
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __( 'Template', 'epfl' ) }>
-                        <RadioControl
-                            label={ __("Select a template", 'epfl') }
-                            selected={ attributes.template }
-                            options={ optionsTemplatesList }
-                            onChange={ template => setAttributes( { template } ) }
-	                    />
-                        <RangeControl
-                            label={ __("Select the number of events", 'epfl') }
-                            value={ attributes.nbEvents }
-                            onChange={ nbEvents  => setAttributes( { nbEvents } ) }
-                            min={ 0 }
-                            max={ 20 }
-                            beforeIcon="arrow-down"
-                            afterIcon="arrow-up"
-                        />
-                    </PanelBody>
-                    <PanelBody title={ __( 'Language', 'epfl' ) }>
-                        <RadioControl
-                            label={ __("Select a language", 'epfl') }
-                            help={ __("The language used to render news results", 'epfl') }
-                            selected={ attributes.lang }
-                            options={ optionsLanguagesList }
-                            onChange={ lang => setAttributes( { lang } ) }
-	                    />
-                    </PanelBody>
-                    <PanelBody title={ __('Period', 'epfl') }>
-                        <RadioControl
-                            label={ __("Select a period", 'epfl') }
-                            selected={ attributes.period }
-                            options={ optionsPeriodsList }
-                            onChange={ period => setAttributes( { period } ) }
-	                      />
-                        { filterPastEventsByYear }
-                    </PanelBody>
-                    <PanelBody title={ __( 'Categories', 'epfl') }>
-                        <PanelRow>
-                        <Select
-                            id='epfl-memento-categories'
-                            name='select-categories'
-                            value={ JSON.parse( attributes.categories ) }
-                            onChange={ handleCategoriesChange }
-                            options={ optionsCategoriesList }
-                            isMulti='true'
-                        />
-                         </PanelRow>
-                    </PanelBody>
-                    <PanelBody title={ __( 'Keyword', 'epfl' ) }>
-                        <TextControl
-                            label={ __("Filter events by keyword", 'epfl') }
-                            value={ attributes.keyword }
-                            onChange={ keyword => setAttributes( { keyword} )}
-                        />
-                    </PanelBody>
+			let optionsYearsList = [
+				{ value: 'no-filter', label: __('No filter', 'epfl') },
+			]
 
-                </InspectorControls>
-            )
-        }
-        return content;
-    }
+			const currentYear = new Date().getFullYear()
+			for (let year = currentYear; year >= 2008; year--) {
+				optionsYearsList.push({ label: year, value: year })
+			}
+
+			let filterPastEventsByYear
+			if (!!attributes.period && attributes.period === 'past') {
+				filterPastEventsByYear = (
+					<SelectControl
+						label={ __('Filter events by year', 'epfl') }
+						value={ attributes.year }
+						options={ optionsYearsList }
+						onChange={ year => setAttributes({ year }) }
+					/>
+				)
+			}
+
+			content = (
+				<InspectorControls>
+					<p><a className="wp-block-help"
+						  href={ __('https://www.epfl.ch/campus/services/website/memento-en/', 'epfl') }
+						  target="new">{ __('Online help', 'epfl') } </a></p>
+					<p className="wp-block-help">{ version }</p>
+					<PanelBody title={ __('Memento', 'epfl') }>
+						<SelectControl
+							label={ __('Select your memento', 'epfl') }
+							help={ __('The events come from the application memento.epfl.ch. If you don\'t have a memento, please send a request to 1234@epfl.ch', 'epfl') }
+							value={ attributes.memento }
+							options={ optionsMementosList }
+							onChange={ memento => {
+								memento = Number(memento)
+								setAttributes({ memento })
+							} }
+						/>
+					</PanelBody>
+					<PanelBody title={ __('Template', 'epfl') }>
+						<RadioControl
+							label={ __('Select a template', 'epfl') }
+							selected={ attributes.template }
+							options={ optionsTemplatesList }
+							onChange={ template => setAttributes({ template }) }
+						/>
+						<RangeControl
+							label={ __('Select the number of events', 'epfl') }
+							value={ attributes.nbEvents }
+							onChange={ nbEvents => setAttributes({ nbEvents }) }
+							min={ 0 }
+							max={ 20 }
+							beforeIcon="arrow-down"
+							afterIcon="arrow-up"
+						/>
+					</PanelBody>
+					<PanelBody title={ __('Language', 'epfl') }>
+						<RadioControl
+							label={ __('Select a language', 'epfl') }
+							help={ __('The language used to render news results', 'epfl') }
+							selected={ attributes.lang }
+							options={ optionsLanguagesList }
+							onChange={ lang => setAttributes({ lang }) }
+						/>
+					</PanelBody>
+					<PanelBody title={ __('Period', 'epfl') }>
+						<RadioControl
+							label={ __('Select a period', 'epfl') }
+							selected={ attributes.period }
+							options={ optionsPeriodsList }
+							onChange={ period => setAttributes({ period }) }
+						/>
+						{ filterPastEventsByYear }
+					</PanelBody>
+					<PanelBody title={ __('Categories', 'epfl') }>
+						<PanelRow>
+							<Select
+								id="epfl-memento-categories"
+								name="select-categories"
+								value={ JSON.parse(attributes.categories) }
+								onChange={ handleCategoriesChange }
+								options={ optionsCategoriesList }
+								isMulti="true"
+							/>
+						</PanelRow>
+					</PanelBody>
+					<PanelBody title={ __('Keyword', 'epfl') }>
+						<TextControl
+							label={ __('Filter events by keyword', 'epfl') }
+							value={ attributes.keyword }
+							onChange={ keyword => setAttributes({ keyword }) }
+						/>
+					</PanelBody>
+
+				</InspectorControls>
+			)
+		}
+		return content
+	}
 }
