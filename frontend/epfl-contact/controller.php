c933731848018d06def90a10b34b6c035fafe307
Xurxo Adrián <41774881+xentenza@users.noreply.github.com>
2022-09-21 08:59:54 +0200

    Update frontend/epfl-contact/controller.php
    Co-authored-by: jdelasoie <julien.delasoie@epfl.ch>

--- frontend/epfl-contact/controller.php
+++ frontend/epfl-contact/controller.php
@@ -26,7 +26,7 @@ function epfl_contact_block($attributes, $inner_content) {
     ob_start();
 ?>
 
-    <div class="container-full my-5 <?php if ($gray_wrapper) : ?>bg-gray-100 py-5<?php endif; ?>">
+    <div class="container-full my-5<?php if ($gray_wrapper) : ?> bg-gray-100 py-5<?php endif; ?>">
       <div class="container <?php if ($gray_wrapper): ?>bg-white p-4 p-md-5<?php endif; ?>">
       <div class="row">
         <div class="col-lg-6">
