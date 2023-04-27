1b52ee757c671c93ef9cf2be84801336ae5ba040
Julien Delasoie <julien.delasoie@epfl.ch>
2023-04-27 15:55:00 +0200

    Fix people block crashing when using a $column value different than 1 or 3

--- frontend/epfl-people/view.php
+++ frontend/epfl-people/view.php
@@ -24,7 +24,7 @@
 
         // Calling the  epfl_dynamic_people_card function directly if selected.
         if ( ALPHABETICAL_ORDER === $order && $columns === '4') {
-            $markup = epfl_dynamic_people_card($persons, $from, '3', $custom_data, $filtered_fields, $display_options);
+            return epfl_dynamic_people_card($persons, $from, '3', $custom_data, $filtered_fields, $display_options);
         }
 
         /* You may wonder why we don't directly call "epfl_people_card" function with all
@@ -33,19 +33,29 @@
         want to go through documentation. But, I thought it was important to put a comment
         here to avoid others persons to want to simplify this code by directly calling 
         "epfl_people_card" and not understand why it's not working... */
-      
-        else if ( ALPHABETICAL_ORDER === $order ) {
-            $function_to_be_called = __NAMESPACE__ . '\epfl_people_' . $columns;
-            $markup = $function_to_be_called($persons, $from, $title, $display_options);
-        } else if ( HIERARCHICAL_ORDER === $order ) {
-            $function_to_be_called = __NAMESPACE__ . '\epfl_people_hierarchical_' . $columns;
-            $markup = $function_to_be_called($persons, $from, $title, $display_options);
-        } else if ( HIERARCHICAL_ORDER_WITH_TITLE === $order ) {
-          $function_to_be_called = __NAMESPACE__ . '\epfl_people_hierarchical_with_title_' . $columns;
-          $markup = $function_to_be_called($persons, $from, $title, $display_options);
+
+        // here is a protection, so we don't call accidentally an existant function, as it happens sometimes
+        // as only allowed values are 1 and 3, as there is no such function with *_2 or *_4...
+        if ( intval($columns) == 2 || intval($columns) == 4 ) {
+            $columns = 3;
         }
 
-        return $markup;
+        if ( ALPHABETICAL_ORDER === $order ) {
+            $function_to_be_called = __NAMESPACE__ . '\epfl_people_' . $columns;
+            return $function_to_be_called($persons, $from, $title, $display_options);
+        }
+
+        if ( HIERARCHICAL_ORDER === $order ) {
+            $function_to_be_called = __NAMESPACE__ . '\epfl_people_hierarchical_' . $columns;
+            return $function_to_be_called($persons, $from, $title, $display_options);
+        }
+
+        if ( HIERARCHICAL_ORDER_WITH_TITLE === $order ) {
+          $function_to_be_called = __NAMESPACE__ . '\epfl_people_hierarchical_with_title_' . $columns;
+          return $function_to_be_called($persons, $from, $title, $display_options);
+        }
+
+        return Utils::render_user_msg("People block: Error unknown parameter for 'order'");
     }
 
     function epfl_people_1($persons, $from, $title, $display_options) {
