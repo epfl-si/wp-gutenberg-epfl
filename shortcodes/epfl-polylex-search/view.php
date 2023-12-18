51e927f1447fc9ac37b2164d97ddb4beb0eeece2
Rosa Maggi <rosa.maggi@epfl.ch>
2023-12-18 13:37:15 +0100

    [Feature] Add translation "Last Consultation

--- shortcodes/epfl-polylex-search/view.php
+++ shortcodes/epfl-polylex-search/view.php
@@ -145,10 +145,18 @@
                     </div>
                     <?php endif; ?>
                 </div>
-				<div class="lex-row-4 flex-row d-md-flex pt-2 pb-2">
+				<div class="lex-row-2 flex-row d-md-flex pt-2 pb-2">
 					<div class="col-1"></div>
-					<div class="col lex-description"><a class="lex-url" href="<?php echo esc_html($lex->urlLastCons); ?>"><?php echo esc_html($lex->urlLastCons); ?></a></div>
+					<div class="col lex-description"><?php echo htmlspecialchars_decode(esc_html($lex->description)); ?></div>
 				</div>
+				<?php if (esc_html($lex->urlLastCons)): ?>
+					<div class="lex-row-2 flex-row d-md-flex pt-2 pb-2">
+						<div class="col-1"></div>
+						<div class="col lex-urlLastCons">
+							<a class="lex-url" href="<?php echo esc_html($lex->urlLastCons); ?>"><?php _e('Last consultation', 'epfl') ?></a>
+						</div>
+					</div>
+				<?php endif; ?>
             </div>
             <?php endforeach; ?>
         <?php endif; ?>
