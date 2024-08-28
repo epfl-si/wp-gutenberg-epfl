af8403920133614fa59c6034a0c097dc11616802
amine <amine.kabbouch@epfl.ch>
2024-08-28 14:42:42 +0200

    adjusting now the api route to call prod

--- frontend/epfl-student-projects/get-sections-zen.php
+++ frontend/epfl-student-projects/get-sections-zen.php
@@ -5,7 +5,7 @@ use \EPFL\Plugins\Gutenberg\Lib\Utils;
 require_once (dirname(__FILE__) . '/../lib/utils.php');
 
 
-$url = "https://test-sti-zen.epfl.ch/api/public/projects/units";
+$url = "https://sti-zen.epfl.ch/api/public/projects/units";
 
 $response = Utils::zen_api_request($url);
 echo json_encode($response);
