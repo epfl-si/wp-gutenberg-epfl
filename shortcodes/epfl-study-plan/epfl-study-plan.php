b9eb2b32afe8c28d4f3f38817c8cfb6ea95b5113
William Belle <william.belle@gmail.com>
2024-10-16 08:52:54 +0200

    fix: edu url

--- shortcodes/epfl-study-plan/epfl-study-plan.php
+++ shortcodes/epfl-study-plan/epfl-study-plan.php
@@ -12,7 +12,7 @@ function epfl_study_plan_process_shortcode( $atts, $content = null ) {
             'plan' => ''
             ), $atts );
     $plan = sanitize_text_field($atts['plan']);
-    $url = "https://prod-edu.epfl.ch/export/".$plan;
+    $url = "https://edu.epfl.ch/export/".$plan;
     ob_start();
 
 ?>
