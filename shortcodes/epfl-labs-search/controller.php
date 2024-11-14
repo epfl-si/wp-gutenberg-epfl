680708d95c3a82bc74e91a63f062243d4235bfb4
Rosa Maggi <rosa.maggi@epfl.ch>
2024-11-14 17:23:40 +0100

    [fix] rename $sites -> $sitesSorted

--- shortcodes/epfl-labs-search/controller.php
+++ shortcodes/epfl-labs-search/controller.php
@@ -5,34 +5,35 @@ namespace EPFL\Plugins\Shortcodes\EPFLLabSearch;
 /**
  * render the shortcode, mainly a form and his table
  */
-function renderLabsSearch($sitesList, $faculty, $institute, $field) {
+function renderLabsSearch($sites, $faculty, $institute, $field) {
   wp_enqueue_script( 'lib-listjs', plugins_url('lib/list.min.js', dirname(__FILE__)), ['jquery'], 1.5, false);
   wp_enqueue_style( 'epfl-labs-search-css', plugins_url('epfl-labs-search.css', __FILE__),false,'1.1','all');
 
-  $titles = array_column($sitesList, 'title');
+  // because the sites array arrives sorted case sensitively, do a "natural order" to sort case-insensitively.
+  $titles = array_column($sites, 'title');
   natcasesort($titles);
-  $sites = [];
+  $sitesSorted = [];
   foreach ($titles as $index => $title) {
-    $sites[] = $sitesList[$index];
+    $sitesSorted[] = $sites[$index];
   }
 
-  filter_out_unused_language($sites);
+  filter_out_unused_language($sitesSorted);
 
   // are we doing a field of research result ?
   if ( !empty($field)) {
     // Remove all laboratories that are not in the current field of research
-    foreach ($sites as $key => $site) {
+    foreach ($sitesSorted as $key => $site) {
       $tags_in_site = [];
       foreach ($site->tags as $tag) {
         $tags_in_site[] = $tag->name;
       }
       if (!in_array($field, $tags_in_site)) {
-        unset($sites[$key]);
+        unset($sitesSorted[$key]);
       }
     }
   }
-  $combo_list_content = separate_tags_by_type($sites);
-  if ( empty($sites) ) {
+  $combo_list_content = separate_tags_by_type($sitesSorted);
+  if ( empty($sitesSorted) ) {
     // Disallow unknown text in the field var, as it will be shown in first page
     // that's a bad field of research, reset is but keep the empty list
     $field = "";
@@ -40,7 +41,7 @@ function renderLabsSearch($sitesList, $faculty, $institute, $field) {
     set_query_var('epfl_labs-predefined_field', $field);
   }
 
-  set_query_var('epfl_labs-sites', $sites);
+  set_query_var('epfl_labs-sites', $sitesSorted);
   set_query_var('epfl_labs-predefined_faculty', $faculty);
   set_query_var('epfl_labs-predefined_institute', $institute);
   set_query_var('eplf_labs-combo_list_content', $combo_list_content);
