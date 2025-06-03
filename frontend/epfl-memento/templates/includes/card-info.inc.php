99d0bb195442e43e1c76d848d3ab68df3a6cfb72
Jch4ipas <jeremy.wuersch@gmail.com>
2025-06-03 16:04:39 +0200

    [fix] change what is bold

--- frontend/epfl-memento/templates/includes/card-info.inc.php
+++ frontend/epfl-memento/templates/includes/card-info.inc.php
@@ -50,17 +50,17 @@ if ($event->url_online_room !== '') {
 }
 
 if (get_current_language() == 'fr' and $event->category->fr_label !== ''){
-    $markup .= __('Category', 'epfl') . ': <b>' . esc_html($event->category->fr_label) . '</b>';
+    $markup .='<b>' . __('Category', 'epfl') . ':</b> ' . esc_html($event->category->fr_label);
     $markup .= '<br>';
 } elseif ($event->category->en_label !== '') {
-    $markup .= __('Category', 'epfl') . ': <b>' . esc_html($event->category->en_label) . '</b>';
+    $markup .='<b>' . __('Category', 'epfl') . ':</b> ' . esc_html($event->category->en_label);
     $markup .= '<br>';
 }
 
 if (get_current_language() == 'fr' and $event->vulgarization->fr_label !== ''){
-    $markup .= __('Public cible', 'epfl') . ': <b>' . esc_html($event->vulgarization->fr_label) . '</b>';
+    $markup .= '<b>' . __('Target audience', 'epfl') . ': </b>' . esc_html($event->vulgarization->fr_label);
     $markup .= '<br>';
 } elseif ($event->vulgarization->en_label !== '') {
-    $markup .= __('Target audience', 'epfl') . ': <b>' . esc_html($event->vulgarization->en_label) . '</b>';
+    $markup .= '<b>' . __('Target audience', 'epfl') . ': </b>' . esc_html($event->vulgarization->en_label);
     $markup .= '<br>';
 }
