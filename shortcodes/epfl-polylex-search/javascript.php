15874961ce6b7038ba0ee49144d59d21806ed7fd
Saskya Panchaud <saskya.panchaud@epfl.ch>
2026-01-26 16:35:15 +0100

    [feature] Display only subcategories linked to the selected category

--- shortcodes/epfl-polylex-search/javascript.php
+++ shortcodes/epfl-polylex-search/javascript.php
@@ -11,6 +11,9 @@
 <script type='text/javascript'>
 window.onload = function() {  // wait that jQuery is loaded
     jQuery(document).ready(function( $ ) {
+        var mapCatSubcat = <?php echo json_encode($cat_with_sub_tree); ?>;
+        var sortedUniqueSubcategories = [...new Set(Object.values(mapCatSubcat).flat())].sort();
+
         var options = {
             valueNames: [
                 'lex-type',
@@ -24,6 +27,28 @@ window.onload = function() {  // wait that jQuery is loaded
         };
 
         var lexList = new List('lexes-list', options);
+
+        function updateSubcategoryOptions() {
+            const selectedCategory = $('#select-category').val();
+            const subcategorySelect = $('#select-subcategory');
+            const selectedSubcategory = <?php echo json_encode($predefined_subcategory); ?>;
+
+            subcategorySelect.find('option:not([value="all"])').remove();
+
+            let subcategories = [];
+            if (selectedCategory === 'all') {
+                subcategories = sortedUniqueSubcategories;
+            } else {
+                const filtredSubcategories = mapCatSubcat[selectedCategory] || [];
+                subcategories = [...new Set(filtredSubcategories)].sort();
+            }
+
+            subcategories.forEach(sub => {
+                subcategorySelect.append($('<option>', { value: sub, text: sub }));
+            });
+
+            subcategorySelect.val(subcategories.includes(selectedSubcategory) ? selectedSubcategory : 'all');
+        }
 
         function applyFilters() {
             const selectedCategory = $('#select-category').val();
@@ -64,17 +89,18 @@ window.onload = function() {  // wait that jQuery is loaded
 
         function onFiltersChanged(changedElement) {
             if (changedElement.id === 'select-category') {
-                document.getElementById('select-subcategory').value = 'all';
+                updateSubcategoryOptions();
             }
             applyFilters();
             updateQueryStringParam();
         }
 
+        updateSubcategoryOptions();
+        applyFilters();
+
         $('#select-category, #select-subcategory, #select-type').on('change', function () {
             onFiltersChanged(this);
         });
-
-        applyFilters();
 
         <?php if (!empty($predefined_search)): ?>
         $('#lexes-search-input').val("<?php echo $predefined_search ?>");
